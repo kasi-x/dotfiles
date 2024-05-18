4def2664da97c750210962c43a14efdd07aeb1e1
kashi-x <kashimiya.exe@gmail.com>
2024-05-19 04:57:49 +0900

    update ubuntu

--- dots/config/nvim/lua/plugins/common/action.lua
+++ dots/config/nvim/lua/plugins/common/action.lua
@@ -46,185 +46,185 @@ return {
         "<leader>k",
         "<cmd>ISwap<cr>",
         desc = "iswap",
-        mode = { "n", "v", "o" },
+        mode = { "n" },
       },
       {
         "<leader>m",
         "<cmd>ISwapNode<cr>",
         desc = "iswapNode",
-        mode = { "n", "v", "o" },
+        mode = { "n" },
+      },
+    },
+  },
+  { --}}}
+    "gbprod/yanky.nvim", --{{{
+    event = "VeryLazy",
+    config = function()
+      require("yanky").setup({
+        ring = {
+          history_length = 100,
+          storage = "shada",
+          sync_with_numbered_registers = true,
+          cancel_event = "update",
+        },
+        system_clipboard = {
+          sync_with_ring = true,
+        },
+      })
+    end,
+    keys = {
+      {
+        "v",
+        "<Plug>(YankyPutAfter)",
+        desc = "yankyAfter",
+        mode = { "n", "v" },
+      },
+      {
+        "V",
+        "<Plug>(YankyPutBefore)",
+        desc = "yankyBefore",
+        mode = { "n", "v" },
+      },
+      {
+        "<C-v>",
+        "<Plug>(YankyCycleForward)",
+        desc = "yankyCycleForward",
+        mode = { "n", "v" },
+      },
+      {
+        "<C-c>",
+        "<Plug>(YankyCycleBackward)",
+        desc = "yankyCycleBackward",
+        mode = { "n", "v" },
+      },
+      {
+        "gv",
+        "<Plug>(YankyGPutAfter)",
+        desc = "GyankyAfter",
+        mode = { "n", "v" },
+      },
+      {
+        "gV",
+        "<Plug>(YankyGPutBefore)",
+        desc = "GyankyBefore",
+        mode = { "n", "v" },
+      },
+      {
+        "<A-v>",
+        "<Plug>(YankyPutAfterBlockwiseJoined)",
+        desc = "BlockwiseJoined",
+        mode = { "n", "v" },
+      },
+      {
+        "<A-c>",
+        "<Plug>(YankyPutAfterBlockwise)",
+        desc = "GyankyAfter",
+        mode = { "n", "v" },
       },
     },
   },
   -- { --}}}
-  --   "gbprod/yanky.nvim", --{{{
-  --   event = "VeryLazy",
-  --   config = function()
-  --     require("yanky").setup({
-  --       ring = {
-  --         history_length = 100,
-  --         storage = "shada",
-  --         sync_with_numbered_registers = true,
-  --         cancel_event = "update",
-  --       },
-  --       system_clipboard = {
-  --         sync_with_ring = true,
-  --       },
-  --     })
-  --   end,
-  --   keys = {
-  --     {
-  --       "v",
-  --       "<Plug>(YankyPutAfter)",
-  --       desc = "yankyAfter",
-  --       mode = { "n", "v" },
-  --     },
-  --     {
-  --       "V",
-  --       "<Plug>(YankyPutBefore)",
-  --       desc = "yankyBefore",
-  --       mode = { "n", "v" },
-  --     },
-  --     {
-  --       "<C-v>",
-  --       "<Plug>(YankyCycleForward)",
-  --       desc = "yankyCycleForward",
-  --       mode = { "n", "v" },
-  --     },
-  --     {
-  --       "<C-c>",
-  --       "<Plug>(YankyCycleBackward)",
-  --       desc = "yankyCycleBackward",
-  --       mode = { "n", "v" },
-  --     },
-  --     {
-  --       "gv",
-  --       "<Plug>(YankyGPutAfter)",
-  --       desc = "GyankyAfter",
-  --       mode = { "n", "v" },
-  --     },
-  --     {
-  --       "gV",
-  --       "<Plug>(YankyGPutBefore)",
-  --       desc = "GyankyBefore",
-  --       mode = { "n", "v" },
-  --     },
-  --     {
-  --       "<A-v>",
-  --       "<Plug>(YankyPutAfterBlockwiseJoined)",
-  --       desc = "BlockwiseJoined",
-  --       mode = { "n", "v" },
-  --     },
-  --     {
-  --       "<A-c>",
-  --       "<Plug>(YankyPutAfterBlockwise)",
-  --       desc = "GyankyAfter",
-  --       mode = { "n", "v" },
-  --     },
+  --   "ThePrimeagen/refactoring.nvim", --{{{
+  --   lazy = "VeryLazy",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
   --   },
+  --   config = function()
+  --     require("refactoring").setup()
+  --     require("telescope").load_extension("refactoring")
+  --   end,
+  --   keys = { --{{{
+  --     {
+  --       "<leader>rr",
+  --       "<CMD>lua require'telescope'.extensions.refactoring.refactors()<CR>",
+  --       desc = "refactoring",
+  --       mode = { "n", "x" },
+  --     },
+  --     {
+  --       "<leader>rF",
+  --       function()
+  --         require("refactoring").refactor("Extract Function")
+  --       end,
+  --       desc = "Extract Function",
+  --       mode = { "x" },
+  --     },
+  --     {
+  --       "<leader>rt",
+  --       function()
+  --         require("refactoring").refactor("Extract Function To File")
+  --       end,
+  --       desc = "Extract Function To File",
+  --       mode = { "x" },
+  --     },
+  --     {
+  --       "<leader>rV",
+  --       function()
+  --         require("refactoring").refactor("Extract Variable")
+  --       end,
+  --       desc = "Extract Variable",
+  --       mode = { "x" },
+  --     },
+  --     {
+  --       "<leader>rf",
+  --       function()
+  --         require("refactoring").refactor("Inline Function")
+  --       end,
+  --       desc = "Inline Function",
+  --       mode = { "n" },
+  --     },
+  --     {
+  --       "<leader>rv",
+  --       function()
+  --         require("refactoring").refactor("Inline Variable")
+  --       end,
+  --       desc = "Inline Variable",
+  --       mode = { "n", "x" },
+  --     },
+  --     {
+  --       "<leader>rb",
+  --       function()
+  --         require("refactoring").refactor("Extract Block")
+  --       end,
+  --       desc = "Extract Block",
+  --       mode = { "n" },
+  --     },
+  --     {
+  --       "<leader>rB",
+  --       function()
+  --         require("refactoring").refactor("Extract Block To File")
+  --       end,
+  --       desc = "Extract Block To File",
+  --       mode = { "n" },
+  --     },
+  --     {
+  --       "<leader>rp",
+  --       function()
+  --         require("refactoring").debug.printf({ below = false })
+  --       end,
+  --       desc = "Print Function",
+  --       mode = { "n" }, -- This remap must be made in normal mode
+  --     },
+  --     {
+  --       "<leader>rP",
+  --       function()
+  --         require("refactoring").debug.print_var()
+  --       end,
+  --       desc = "Print Variable",
+  --       mode = { "n", "x" }, -- Supports both visual and normal mode
+  --     },
+  --     {
+  --       "<leader>rc",
+  --       function()
+  --         require("refactoring").debug.cleanup({})
+  --       end,
+  --       desc = "Cleanup Refactoring",
+  --       mode = { "n" }, -- Supports only normal mode
+  --     },
+  --   }, --}}},
   -- },
   { --}}}
-    "ThePrimeagen/refactoring.nvim", --{{{
-    lazy = "VeryLazy",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("refactoring").setup()
-      require("telescope").load_extension("refactoring")
-    end,
-    keys = { --{{{
-      {
-        "<leader>rr",
-        "<CMD>lua require'telescope'.extensions.refactoring.refactors()<CR>",
-        desc = "refactoring",
-        mode = { "n", "x" },
-      },
-      {
-        "<leader>rF",
-        function()
-          require("refactoring").refactor("Extract Function")
-        end,
-        desc = "Extract Function",
-        mode = { "x" },
-      },
-      {
-        "<leader>rt",
-        function()
-          require("refactoring").refactor("Extract Function To File")
-        end,
-        desc = "Extract Function To File",
-        mode = { "x" },
-      },
-      {
-        "<leader>rV",
-        function()
-          require("refactoring").refactor("Extract Variable")
-        end,
-        desc = "Extract Variable",
-        mode = { "x" },
-      },
-      {
-        "<leader>rf",
-        function()
-          require("refactoring").refactor("Inline Function")
-        end,
-        desc = "Inline Function",
-        mode = { "n" },
-      },
-      {
-        "<leader>rv",
-        function()
-          require("refactoring").refactor("Inline Variable")
-        end,
-        desc = "Inline Variable",
-        mode = { "n", "x" },
-      },
-      {
-        "<leader>rb",
-        function()
-          require("refactoring").refactor("Extract Block")
-        end,
-        desc = "Extract Block",
-        mode = { "n" },
-      },
-      {
-        "<leader>rB",
-        function()
-          require("refactoring").refactor("Extract Block To File")
-        end,
-        desc = "Extract Block To File",
-        mode = { "n" },
-      },
-      {
-        "<leader>rp",
-        function()
-          require("refactoring").debug.printf({ below = false })
-        end,
-        desc = "Print Function",
-        mode = { "n" }, -- This remap must be made in normal mode
-      },
-      {
-        "<leader>rP",
-        function()
-          require("refactoring").debug.print_var()
-        end,
-        desc = "Print Variable",
-        mode = { "n", "x" }, -- Supports both visual and normal mode
-      },
-      {
-        "<leader>rc",
-        function()
-          require("refactoring").debug.cleanup({})
-        end,
-        desc = "Cleanup Refactoring",
-        mode = { "n" }, -- Supports only normal mode
-      },
-    }, --}}},
-  },
-  { --}}}
-    "WWansmer/treesj", --{{{
+    "Wansmer/treesj", --{{{
     keys = {
       {
         "<leader>j",
@@ -261,9 +261,7 @@ return {
       },
       {
         "lG",
-        function()
-          require("neogen").generate({ type = "class" })
-        end,
+        function() require("neogen").generate({ type = "class" }) end,
         mode = "n",
         desc = "Neogen Class Comment",
       },
